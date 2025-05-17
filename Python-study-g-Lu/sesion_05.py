8eaa2ce70a21b04bd56053c0679d3b2f19058d6c
luisenrique0508 <162665871+luisenrique0508@users.noreply.github.com>
2025-05-17 16:44:30 -0400

    ejer de ejercicios de la session 1 al 5 por ahora

---
+++ Python-study-g-Lu/sesion_05.py
@@ -0,0 +1,86 @@
+print(type(1))
+print("##############")
+variable = 100
+print(variable)
+print(type(variable))
+print("##############")
+variable_2 = int (20)
+print (variable_2)
+print ( type (variable_2) )
+print("##############")
+print ("Base decimal")
+print (10)
+# Valor 10 en binario
+print ("Base binaria")
+print (0b1010)
+# Valor 10 en octal
+print ("Base octal")
+print (0o12)
+# Valor 10 en hexadecimal
+print ("Base hexadecimal")
+print (0xa)
+print ("##############")
+variable_3 = 123456789012345678901234567890123456789012345678901234567890038474292837362384726323756749857203475236628348235723720303475238423565238462345237423492364253473392837645263328420010434100103453264219730256263545235291
+print (variable_3)
+print (type (variable_3))
+print("##############")
+print (0.5)
+print ( type (0.5) )
+print ("##############")
+variable_4 = 0.100546
+print (variable_4)
+print ( type (variable_4) )
+variable_7 = float (1)
+print(variable_7)
+print ( type (variable_7) )
+print ("##############")
+variable_6 = 2.0e-3
+print(variable_6)
+print ( type (variable_6) )
+print ("##############")
+variable_5 = 0.9999999999999999
+print(variable_5)
+print ( type (variable_5) )
+print ("##############")
+a = 10
+b = 3
+# Suma
+print ("Suma")
+print (a + b)
+# Resta
+print ("Resta")
+print (a - b)
+# Multiplicación
+print ("Multiplicación")
+print (a * b)
+# División
+print ("División")
+print (a / b)
+# Potencia
+print ("Potencia")
+print (a ** b)
+# Módulo o residuo
+print ("Módulo o residuo")
+print (a % b)
+# División entera
+print ("División entera")
+print (a // b)
+print ("##############")
+print ("Operadores de comparación")
+comparar = 10
+print (comparar < 10)
+print (comparar > 10)
+print (comparar == 10)
+print (comparar <= 10)
+print (comparar >= 10)
+print (comparar != 10)
+print ("##############")
+print ("Operadores de comparación con int - float")
+entero = 10
+flotante = 10.0
+print (entero < flotante)
+print (entero > flotante)
+print (entero == flotante)
+print (entero <= flotante)
+print (entero >= flotante)
+print (entero != flotante)
